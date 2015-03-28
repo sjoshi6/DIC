23964a23941b6c1078c8d25cde1757a2c9d9e811
Saurabhh <sjoshi6@ncsu.edu>
2015-03-28 12:22:19 -0400

    routes dictionary

--- NodeJs_Server/routes.js
+++ NodeJs_Server/routes.js
@@ -2,6 +2,8 @@ var request = require('request');
 
 module.exports = function(app) {
 
+var hash_counter={}
+
 app.post('/pushjson',function(req,res){
 
                               console.log(req.body.entities.hashtags);
@@ -9,7 +11,20 @@ app.post('/pushjson',function(req,res){
 
                               hashtags_arr.forEach(function(data_json){
                                 console.log(data_json.text)
+                                var tweet_hash_text=data_json.text
+
+                                if(hash_counter.tweet_hash_text)
+                                {
+                                    hash_counter.tweet_hash_text = hash_counter.tweet_hash_text + 1
+                                }
+                                else
+                                {
+                                  hash_counter.tweet_hash_text = 1
+                                }
+
                               });
+
+                              console.log(hash_counter);
                               
                               res.json('');
 
