4bf82ad6fd9e6b16dcad1129a4694e7cb225ce24
Saurabhh <sjoshi6@ncsu.edu>
2015-03-28 12:12:56 -0400

    hashtags spelling fix

--- NodeJs_Server/routes.js
+++ NodeJs_Server/routes.js
@@ -5,7 +5,7 @@ module.exports = function(app) {
 app.post('/pushjson',function(req,res){
 
                               console.log(req.body.entities.hashtags);
-                              var hastags_arr=req.body.entities.hashtags
+                              var hashtags_arr=req.body.entities.hashtags
 
                               hashtags_arr.forEach(function(data_json){
                                 console.log(data_json.text)
