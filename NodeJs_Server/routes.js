8811353f7186fc99ee3318d240d65be5344c34d9
Saurabhh <sjoshi6@ncsu.edu>
2015-03-27 22:48:40 -0400

    routes

--- NodeJs_Server/routes.js
+++ NodeJs_Server/routes.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
 
 app.post('/pushjson',function(req,res){
 
-                              console.log(req.body.entities)
+                              console.log(req.body.hashtags)
                               console.log(req.body.entities['hashtags']);
                               res.json('');
 
