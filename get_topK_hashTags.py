d31aa5283280fbb74b9811f8d251026871bacbd8
Saurabhh <sjoshi6@ncsu.edu>
2015-03-29 22:58:35 -0400

    Print errors resolved

--- get_topK_hashTags.py
+++ get_topK_hashTags.py
@@ -2,14 +2,14 @@ import json
 import requests
 import sys
 
-def printTopK(self,k):
+def printTopK(k):
     topk=k
     response=requests.get('http://localhost:8181/tweet_hash_counter')
     data_topK=json.loads(response.text)
     asc_sorted = sorted(data_topK.iteritems(), key=lambda x:-x[1])[:topk]
 
     print('======================')
-    print('Top '+topk+' HashTags are:\n')
+    print('Top '+str(topk)+' HashTags are:\n')
     for elem in asc_sorted:
          print("{0}: {1}".format(*elem))
     print('======================')
