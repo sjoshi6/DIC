8125f50de0d1b1ef0859f1b52ba1db269d4b7be7
Saurabhh <sjoshi6@ncsu.edu>
2015-04-07 21:23:24 -0400

    change of structure

---
+++ app.js
@@ -0,0 +1,20 @@
+var express  = require('express');
+var connect = require('connect');
+var app      = express();
+var port     = process.env.PORT || 8181;
+
+// Configuration
+app.use(connect.logger('dev'));
+app.use(connect.json());
+app.use(connect.urlencoded());
+
+var routedata = require('./routes.js')
+routedata(app);
+
+var server=app.listen(port, function () {
+
+	  var host = server.address().address
+	  var port = server.address().port
+
+   	console.log('Example app listening at http://%s:%s', host, port)
+});
---
+++ routes.js
@@ -0,0 +1,106 @@
+var request = require('request');
+
+module.exports = function(app) {
+
+var hash_counter={}
+var slidingwindow=[]
+
+var args = process.argv.slice(2);
+var topk = args[0]
+var window_size = args[1]
+
+console.log('topk is'+topk);
+console.log('window size is'+window_size);
+
+
+app.post('/pushjson',function(req,res){
+
+                              //extract the hashtag array from message
+                              console.log(req.body.entities.hashtags);
+                              var hashtags_arr=req.body.entities.hashtags
+
+                              //untill window reaches windowsize simply add elements to slider window.
+                              if(slidingwindow.length < (window_size+1))
+                              {
+                                slidingwindow.push(hashtags_arr)
+                              }
+                              else
+                              {
+                                //as soon as the window crosses window size. Remove first element
+                                var popped_list = slidingwindow.shift()
+
+                                console.log('popped list')
+                                console.log(popped_list)
+                                //iterate over all the hashtags in popped element and decrement the hashcount
+                                popped_list.forEach(function(data){
+                                        hash_counter[data.text] = hash_counter[data.text] - 1
+
+                                        if(hash_counter[data.text] == 0)
+                                        {
+                                          hash_counter[data.text] = null
+                                          delete hash_counter[data.text]
+                                        }
+                                });
+                                // Add the element at the end of slider window
+                                slidingwindow.push(hashtags_arr)
+
+                              }
+
+                                hashtags_arr.forEach(function(data_json){
+                              //  console.log(data_json.text)
+                                var tweet_hash_text=data_json.text
+
+                                if(hash_counter[tweet_hash_text])
+                                {
+                                    hash_counter[tweet_hash_text] = hash_counter[tweet_hash_text] + 1
+                                }
+                                else
+                                {
+                                  hash_counter[tweet_hash_text] = 1
+                                }
+
+                              });
+
+                            //  console.log(hash_counter);
+
+                              res.json('');
+
+});
+
+app.get('/tweet_hash_counter',function(req,res){
+          //Reply back the current hashmap
+          var json_reply = hash_counter;
+          res.json(json_reply);
+})
+
+app.get('/get_top_k_tweets',function(req,res){
+
+        var reply_arr = []
+
+        for(keyy in hash_counter)
+        {
+          reply_arr.push([keyy,hash_counter[keyy]])
+        }
+
+        reply_arr.sort(function(a,b){return a[1]-b[1] });
+        reply_arr.reverse();
+
+        var k=topk
+
+        if(reply_arr.length < k)
+        {
+          k=reply_arr.length
+        }
+
+       var send_arr=reply_arr.slice(0,k);
+       var string_reply ="Top "+topk+" hashtags are : <br> =============== <br>"
+
+       send_arr.forEach(function(data){
+         string_reply=string_reply+"<br>"+String(data)
+       })
+
+        res.send(string_reply);
+});
+
+
+}
