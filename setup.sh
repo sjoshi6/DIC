728eeadd8db035609dc69d8147a80bba69eea4b4
Saurabhh <sjoshi6@ncsu.edu>
2015-04-07 21:34:53 -0400

    setup file

---
+++ setup.sh
@@ -0,0 +1,13 @@
+sudo apt-get update -y
+sudo apt install curl -y
+sudo apt-get install python-pip python-dev build-essential -y
+sudo pip install --upgrade pip -y
+sudo pip install --upgrade virtualenv -y
+sudo apt install npm -y
+sudo apt-get install npm -y
+sudo apt-get install nodejs-legacy -y
+sudo pip install tweepy -y
+npm install
+sudo ln -s /usr/bin/nodejs /usr/sbin/node
+
+
