1d5e654068c1fcf01900d5509ca93c226571fb94
Saurabhh <sjoshi6@ncsu.edu>
2015-04-07 21:50:28 -0400

    changes

--- setup.sh
+++ setup.sh
@@ -1,13 +1,21 @@
+#updates
 sudo apt-get update -y
+#install curl
 sudo apt install curl -y
-sudo apt-get install python-pip python-dev build-essential -y
-sudo pip install --upgrade pip -y
-sudo pip install --upgrade virtualenv -y
+#install python and other dependancies
+sudo apt-get install python-pip python-dev build-essential
+#install pip
+sudo pip install --upgrade pip
+sudo pip install --upgrade virtualenv
+#install npm
 sudo apt install npm -y
 sudo apt-get install npm -y
+#install node
 sudo apt-get install nodejs-legacy -y
-sudo pip install tweepy -y
+sudo pip install tweepy
+#install from package json
 npm install
+# updates symlinks
 sudo ln -s /usr/bin/nodejs /usr/sbin/node
 
 
